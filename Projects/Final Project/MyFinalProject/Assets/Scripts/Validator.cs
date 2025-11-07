465e94c8a2efa72a1a36ad3ef38c9202e83ecc29
2101-2099 <21012099@sfcollege.edu>
2025-11-06 19:37:53 -0500

    End of Class

--- Projects/Final Project/MyFinalProject/Assets/Scripts/Validator.cs
+++ Projects/Final Project/MyFinalProject/Assets/Scripts/Validator.cs
@@ -5,6 +5,7 @@ public class Validator : MonoBehaviour
     //part of player object
     //player has the validator with box collider
     //checks if it is a valid place to plant
+    //sprite renderer object
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
